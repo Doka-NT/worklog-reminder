813a1c900d334e5fc57f5464bc4f577b69598ee5
Soshnikov Artem <213036@skobka.com>
2021-08-08 19:37:23 +0500

    Cross-platform tray window positioning

--- src/UI/MainWindow.js
+++ src/UI/MainWindow.js
@@ -1,7 +1,8 @@
 import {BrowserWindow} from 'electron'
+import positioner from 'electron-traywindow-positioner'
 
-class MainWindow extends BrowserWindow {
-
+class MainWindow extends BrowserWindow
+{
     /**
      * @param {WindowManager} windowManager
      * @param {object} options
@@ -12,32 +13,18 @@ class MainWindow extends BrowserWindow {
     }
 
     show() {
-        const position = this._getWindowPosition()
+        positioner.position(this, this.windowManager.getTray().tray.getBounds())
 
-        this.setPosition(position.x, position.y, false)
         super.show()
         this.focus()
     }
 
     setHandlers() {
-        this.on('blur', () => { 
+        this.on('blur', () => {
             if (!this.webContents.isDevToolsOpened()) {
                 this.hide()
             }
          })
-    }
-
-    _getWindowPosition() {
-        const windowBounds = this.getBounds()
-        const trayBounds = this.windowManager.getTray().tray.getBounds()
-
-        // Center window horizontally below the tray icon
-        const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
-
-        // Position window 4 pixels vertically below the tray icon
-        const y = Math.round(trayBounds.y + trayBounds.height + 4)
-
-        return {x: x, y: y}
     }
 }
 
