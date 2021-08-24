4bf2efcfc8e42fd41225c2223674ae9f52d0a7dc
Soshnikov Artem <213036@skobka.com>
2021-08-24 19:30:43 +0500

    delete old files

--- src/____Renderer/Screen.js
+++
@@ -1,8 +0,0 @@
-class SCREEN_DICT {
-    static WELCOME = 'welcome';
-    static ACCESS_TOKEN = 'access-token';
-    static CHECK_TOKEN = 'check-token';
-    static ISSUES = 'issues';
-}
-
-export default SCREEN_DICT;
