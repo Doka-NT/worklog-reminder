889bb6647b60094e55153eede0bab3f0e236b9be
Soshnikov Artem <213036@skobka.com>
2021-08-30 23:58:40 +0500

    Capitalize components directories

---
+++ src/Application/Renderer/Screen/WelcomeScreen/Components/NavButtons.js
@@ -0,0 +1,17 @@
+import NextButton from './NextButton';
+import PrevButton from './PrevButton';
+
+export default function NavButtons(_props) {
+  const _defaults = {
+    nextBtnProps: {},
+  };
+
+  const props = { ..._defaults, ..._props };
+
+  return (
+    <div className="buttons">
+      <PrevButton />
+      <NextButton {...props.nextBtnProps} />
+    </div>
+  );
+}
