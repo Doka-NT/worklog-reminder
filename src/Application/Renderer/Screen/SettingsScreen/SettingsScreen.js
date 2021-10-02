c9afee883869342e0ec0e36d72cdab7a689906a8
Soshnikov Artem <213036@skobka.com>
2021-10-02 22:24:37 +0500

    #13 remove unused code

--- src/Application/Renderer/Screen/SettingsScreen/SettingsScreen.js
+++ src/Application/Renderer/Screen/SettingsScreen/SettingsScreen.js
@@ -1,21 +1,11 @@
 import 'onsenui';
 import { List, Page } from 'react-onsenui';
-import { useDispatch } from 'react-redux';
-import JiraAPI from '../../../../Infrastructure/JiraAPI/JiraAPI';
-import { showScreen } from '../../Store/appSlice';
-import ScreenDict from '../ScreenDict';
 import CredentialsSettings from './Components/CredentialsSettings';
 import OtherSettings from './Components/OtherSettings';
 import SettingsToolbar from './Components/SettingsToolbar';
 import './style.less';
 
 export default function SettingsScreen() {
-  const dispatch = useDispatch();
-  const onSaveClick = () => {
-    JiraAPI.flushCache();
-    dispatch(showScreen(ScreenDict.CHECK_TOKEN));
-  };
-
   return (
     <section className="screen screen__access-token">
       <Page
